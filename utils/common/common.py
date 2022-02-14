80e0adb0d0f71d447f3294c6d0ba18fa8dde9c03
Kangnam Kim <kangnam7654@naver.com>
2022-02-14 22:31:43 +0900

    [2022.02.14][김강남] 1. initial commit

--- utils/common/common.py
+++ utils/common/common.py
@@ -1,11 +1,17 @@
 import yaml
 from box import Box
+import pandas as pd
 
 
 def load_config(cfg_path=None):
     with open(cfg_path, 'r') as f:
         config = Box(yaml.full_load(f))
     return config
+
+
+def load_csv(path=None):
+    csv = pd.read_csv(path, encoding='utf-8')
+    return csv
 
 
 if __name__ == '__main__':
